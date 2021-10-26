22b4158f9f97caae903312f15866b0d6ca6bf550
ArturMinnullin <minnullin.artur@gmail.com>
2021-10-27 01:40:59 +0300

    Load exchange rate

---
+++ app/javascript/controllers/transaction_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ['loader', 'rate', 'btcInput', 'usdtInput']
+  static values = { rate: Number }
+
+  connect() {
+    fetch('http://api.coinlayer.com/api/live?access_key=617388153a22fa0d941bcc44fc711566&target=BTC&symbols=USDT')
+      .then(response => response.json())
+      .then(data => {
+        this.rateValue = data.rates.USDT
+        this.loaderTarget.classList.add('hidden')
+
+        this.rateTarget.classList.remove('hidden')
+        this.rateTarget.innerText = `1 USDT ~ ${this.rateValue} BTC`
+      })
+  }
+
+  input() {
+    this.btcInputTarget.value = this.usdtInputTarget.value * this.rateValue;
+  }
+}
